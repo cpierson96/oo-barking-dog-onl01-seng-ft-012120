75a1732f0474eafc9f1b52f26ef65c0a76cd86d2
Claudia Pierson <c172@umbc.edu>
2020-02-05 04:14:26 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,10 +1,29 @@
+<<<<<<< HEAD
 class Dog
   attr_accessor :name
 
   def bark
+=======
+class Dog 
+def name=(dogs_name)
+  @this_dogs_name = dogs_name 
+
+end 
+  def name
+    @this_dogs_name
+   
+  end
+end
+  def bark 
+    @this_dogs_name
+>>>>>>> 384da53bb72c0c74491a8a2787d9f5d303894371
     puts "woof!"
   end
 end
 fido=Dog.new
+<<<<<<< HEAD
 fido.bark
 
+=======
+fido.bark
+>>>>>>> 384da53bb72c0c74491a8a2787d9f5d303894371
